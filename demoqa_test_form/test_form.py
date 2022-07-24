cd208e82674791358de8d21fe8fc6ba2ad9ce238
artemiygusev04 <PMPO1002@yandex.ru>
2022-07-24 18:14:55 +0300

    test10

--- demoqa_test_form/test_form.py
+++ demoqa_test_form/test_form.py
@@ -1,5 +1,6 @@
 import allure
 from allure_commons.types import Severity
+from tests.controls.application_manager import app
 from selene.support.shared.jquery_style import s, ss
 from env import *
 from tests.helper.acceptance_test_modul import url_open_size, add_file
@@ -21,11 +22,11 @@ def test_case_practice_form():
         s('//*[@id="userNumber"]').type('4815162342')
         s('//*[@id="currentAddress"]').type('Oceanic')
 
-#    with allure.step('Выбираем элемент в поле Subject'):
-#        app.subject(s(subjects_input)).select_element_in_list('g', select_element_in_subject)
+    with allure.step('Выбираем элемент в поле Subject'):
+        app.subject(s(subjects_input)).select_element_in_list('g', select_element_in_subject)
 
-#    with allure.step('Выбираем текущую дату'):
-#        app.date_picker(s(date_of_birth_input)).select_date_in_datepicker()
+    with allure.step('Выбираем текущую дату'):
+        app.date_picker(s(date_of_birth_input)).select_date_in_datepicker()
 
     with allure.step('Выбираем пол: male'):
         s(gender_select_male).click()
@@ -33,11 +34,11 @@ def test_case_practice_form():
     with allure.step('Выбираем хобби: Спорт'):
         s(hobbies_select_sports).click()
 
- #   with allure.step('Выбираем в списке State: 1 элемент'):
- #       app.drop_down(s(list_state)).select_element_in_dropdown(element_in_list_state)
+    with allure.step('Выбираем в списке State: 1 элемент'):
+        app.drop_down(s(list_state)).select_element_in_dropdown(element_in_list_state)
 
- #   with allure.step('Выбираем в списке City: 1 элемент'):
- #       app.drop_down(s(list_city)).select_element_in_dropdown(element_in_list_city)
+    with allure.step('Выбираем в списке City: 1 элемент'):
+        app.drop_down(s(list_city)).select_element_in_dropdown(element_in_list_city)
 
     with allure.step('Добавляем картинку в поле загрузки файла'):
         add_file(send_picture_button, file_name=file_name)
@@ -45,5 +46,5 @@ def test_case_practice_form():
     with allure.step('Кликаем по кнопки "Отправить форму"'):
         s(send_data).click()
 
-#   with allure.step('Проверяем результаты отправленных данных в таблицe'):
-#        app.check_table_text(ss(table_name)).check_expected_result_in_table(*expected_result_in_table)
+    with allure.step('Проверяем результаты отправленных данных в таблицe'):
+        app.check_table_text(ss(table_name)).check_expected_result_in_table(*expected_result_in_table)
