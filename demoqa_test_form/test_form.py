d0188b6402c0f14d73bb8e8d2fa86cd4ca2360ef
artemiygusev04 <PMPO1002@yandex.ru>
2022-07-24 18:27:08 +0300

    test11

--- demoqa_test_form/test_form.py
+++ demoqa_test_form/test_form.py
@@ -1,8 +1,8 @@
 import allure
 from allure_commons.types import Severity
+from env import *
 from tests.controls.application_manager import app
 from selene.support.shared.jquery_style import s, ss
-from env import *
 from tests.helper.acceptance_test_modul import url_open_size, add_file
 
 
